62a4fb3473b7e6c967da97ea8cf3e79aae38029f
Tanner <tannerfink2000@gmail.com>
2023-05-19 09:52:14 -0700

    fixed connection.js and server

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,10 @@
-// IMPORT required libraries
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
-const mysql = require('mysql2');
 const Sequelize = require('sequelize');
-const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
 
-// .ENV variables
+// Load .env variables
 dotenv.config();
 
-// CREATE instance
-const app = express();
-
-// VIEW for Handlebars.js
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-// MySQL database (Sequelize ORM)
+// MySQL database connection via Sequelize ORM
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
@@ -28,21 +14,10 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     underscored: true,
   },
 });
-module.exports = sequelize;
 
-// TEST connection to the database
+// Test connection to the database
 sequelize.authenticate()
   .then(() => console.log('Database connection successful.'))
   .catch(err => console.error('Unable to connect to the database:', err));
 
-// SET UP auth. (express-session and cookies)
-app.use(cookieParser());
-
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
-
-// Export the app
-module.exports = app;
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const path = require('path'); //UTILITIES directory paths
 const express = require('express'); // EXPRESS.JS import
 const exphbs = require('express-handlebars');// RENDER HTML views
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
 const routes = require('./controllers'); // HANDLES routes for endpoints
 const helpers = require('./utils/helpers'); // HANDLES custom helper for hbs
-const sequelize = require('./config/connection'); //CONNECTION sequelize db
+const sequelize = require('./db/db'); //CONNECTION sequelize db
+
+dotenv.config();
 
 //CREATE instance & PORT/default val
 const app = express();
@@ -11,6 +17,14 @@ const PORT = process.env.PORT || 3001;
 
 // CREATE Handlebars.js object using custom helpers
 const hbs = exphbs.create({ helpers });
+
+app.use(cookieParser());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 
 // NOTIFY Express.js of template selection
 app.engine('handlebars', hbs.engine);
@@ -24,8 +38,15 @@ app.use('/images', express.static('images')); // SERVE static images from the 'i
 
 app.use(routes); //REGISTER defined routes
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: 'An unexpected error occurred' });
+});
+
+//Start server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
 
 
