9c76aef261b3a60567ccae65c26acbb74e79a870
Micci11 <miccibenner@gmail.com>
2023-05-18 20:37:49 -0400

    update

--- public/JAVASCRIPTTEST.js
+++ public/JAVASCRIPTTEST.js
@@ -1,5 +1,3 @@
-
-
 document.getElementById('pictureSelect').addEventListener('change', function() {
     var pictureContainer = document.getElementById('pictureContainer');
   
@@ -12,12 +10,19 @@ document.getElementById('pictureSelect').addEventListener('change', function() {
 
     // Create a new image element and add it to the container
     if (imgSrc) {
+
       var img = document.createElement('img');
       img.src = imgSrc;
       img.classList.add('picture-image');
       pictureContainer.appendChild(img);
+      img.setAttribute('width', 250);
+      //pictureContainer.style.backgroundImage=`url(${imgSrc})`;
     }
   });
+
+
+ 
+  
 
 
 
