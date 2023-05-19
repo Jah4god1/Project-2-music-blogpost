990842712cad7a4bfc25fbffced7bcae196f46b9
Tanner <tannerfink2000@gmail.com>
2023-05-19 07:19:25 -0700

    moved most user routes to home and got rid of song

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,62 +1,86 @@
 const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
+const bcrypt = require('bcrypt');
+const { Post } = require('../models');
 
-// Prevent non-logged-in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((user) => user.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // Pass the logged_in flag to the template
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Render the login page
-router.get('/login', (req, res) => {
-  // If a session exists, redirect the request to the homepage
+// Get the homepage
+router.get('/', async (req, res) => {
+  // If user is logged in, show homepage. If not, show login/register page.
   if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+    try {
+      const userData = await User.findAll({
+        attributes: { exclude: ['password'] },
+        order: [['name', 'ASC']],
+      });
 
-  res.render('login');
+      const users = userData.map((user) => user.get({ plain: true }));
+
+      res.render('homepage', {
+        users,
+        logged_in: req.session.logged_in,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.render('frontpage');
+  }
 });
 
-// Render the register page
-router.get('/register', (req, res) => {
-  // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-
-  res.render('register');
-});
-
-// Handle the registration process
+// Register new user
 router.post('/register', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const { username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ username, email, password: hashedPassword });
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.user_id = user.id;
       req.session.logged_in = true;
-
-      res.redirect('/');
+  
+      res.status(201).json(user);
     });
   } catch (err) {
-    res.status(400).json(err);
+    console.error(err);
+    res.status(500).json({ error: 'Failed to register user' });
+  }
+});
+
+// User login
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) { 
+        return res.status(400).json({ error: 'Invalid email or password' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        return res.status(400).json({ error: 'Invalid email or password' });
+    }
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+  
+      res.status(200).json({ message: 'Logged in successfully' });
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to login' });
   }
 });
 
@@ -68,6 +92,24 @@ router.post('/logout', (req, res) => {
     });
   } else {
     res.status(404).end();
+  }
+});
+
+// Create new post
+router.post('/post', withAuth, async (req, res) => {
+  try {
+    const { title, content } = req.body;
+
+    const newPost = await Post.create({ 
+      title, 
+      content, 
+      userId: req.session.user_id // assuming user's ID is stored in session
+    });
+
+    res.status(201).json(newPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to create post' });
   }
 });
 
