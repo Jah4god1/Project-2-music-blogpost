f1a7113525a512c5ffff984de2619c7325bab52d
Carol Wargo <Carolwargo@gmail.com>
2023-05-22 20:56:40 -0400

    Update password storage jsonwebtoken

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,6 +2,54 @@ const express = require('express');
 const router = express.Router();
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// Secret key for JWT
+const secretKey = 'your-secret-key';
+
+// User registration route
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // Check if the username already exists
+    const existingUser = await User.findOne({ where: { username } });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Username already exists' });
+    }
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Create a new user
+    await User.create({ username, password: hashedPassword });
+    return res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// User login route
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // Find the user by username
+    const user = await User.findOne({ where: { username } });
+    // Check if the user exists
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    // Compare the provided password with the stored hashed password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+    // Generate a JWT token
+    const token = jwt.sign({ username }, secretKey);
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 // Delete user account
 router.delete('/:id', async (req, res) => {
