a685f95a7c188d5140bbf0894b01925d6610d03e
Carol Wargo <Carolwargo@gmail.com>
2023-05-23 18:40:44 -0400

    auth. js and user_routes rewrite

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,109 +1,82 @@
-//bcrypt dependency located in models/user.js 
-//CHECK paths
-const express = require('express');
-const router = express.Router();
-const { User } = require('../../models');
-const { hashPassword, checkPassword } = require('./auth');//ADDED
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
 
-// MIDDLEWARE- authenticate user (NEW)
-const authenticateUser = (req, res, next) => {
+router.get('/', async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('project', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// USE withAuth middleware
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // FIND logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // IF user logged in, redirect the request to another route
   if (req.session.logged_in) {
-    // USER logged in, proceed
-    next();
-  } else {
-    // USER NOT logged in, send error
-    res.status(401).json({ error: 'Unauthorized' });
+    res.redirect('/profile');
+    return;
   }
-};
 
-//CREATE REGISTRATION
-router.post('/', async(req, res) => {
-  try {
-    const hashedPassword = await hashPassword(req.body.password);
-
-    // CREATE NEW
-    const userData = await User.create({
-      email: req.body.email,
-      password: hashedPassword,
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-//LOGIN endpoint (find User)
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({where: {email: req.body.email} });
-
-    if (!userData) {
-      res
-        .status(400)
-        .json ({ message: 'Incorrect email or password, please try again'});
-        return;
-    }
-//COMPARES hashed password against stored password
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or passwork, please try again'});
-      return;
-    }
-
-//UPDATE
-    req.session.save(() =>  {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json ({ user: userData, message: 'You are now logged in!'});
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-//LOGOUT- Requires authentication (NEW)
-router.post('/logout', authenticateUser,(req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-
-// DELETE user account - Requires authentication
-router.delete('/:id', authenticateUser, async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    // Delete the user by their ID
-    await User.destroy({ where: { id } });
-
-    res.status(204).end();
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Failed to delete user' });
-  }
-});
-
-// DELETE a user's post - Requires authentication
-router.delete('/posts/:postId', authenticateUser, async (req, res) => {
-  try {
-    const { postId } = req.params;
-
-    // DELETE post by its ID
-    await Post.destroy({ where: { id: postId } });
-
-    res.status(204).end();
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Failed to delete post' });
-  }
+  res.render('login');
 });
 
 module.exports = router;
