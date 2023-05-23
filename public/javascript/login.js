a7a6709a6f0f56bd63b62822541278469aa0923c
Carol Wargo <Carolwargo@gmail.com>
2023-05-23 17:17:18 -0400

    login, logout & sign up.js redo

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,41 +1,27 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", async function (event) {
-    event.preventDefault();
+const loginFormHandler = async (event) => {
 
-    // Get form input values
-    var username = document.getElementById("inputUsername").value;
-    var password = document.getElementById("inputPassword").value;
+  event.preventDefault();
 
-    // Perform form validation
-    if (username === "" || password === "") {
-      alert("Please enter a username and password");
-      return;
-    }
-
-    const response = await fetch("api/users/login", {
-      method: "POST",
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-      headers: { "Content-Type": "application/json" },
+  // GATHER form elements data
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+  
+  if (email && password) {
+    // SEND  e-mail & password to server
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
-    if (response.ok){
-        document.location.replace("/")
-        
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to log in');
     }
-    else {
-        alert("login failed")
-    }
+  }
+};
 
-    // Perform further processing with the form data (e.g., send it to a server for authentication)
-    // You can make an AJAX request or perform any desired action here
-
-    // Reset the form
-    document.getElementById("loginForm").reset();
-
-    // Display a success message
-    alert("Login successful!");
-  });
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,4 +1,71 @@
-document.getElementById('signupForm').addEventListener('submit', function(event) {
+ // HANDLE form submission function
+function handleSignup(event) {
+    event.preventDefault(); 
+  
+    //GET form inputs by id (CHANGE to: usernameInput, emailInput, passwordInput)
+    const usernameInput = document.getElementById('username');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+  //NEED TO CHANGE IN handlebars if all are ok with eliminating "fullName"
+    
+    // GET User input values (NEW addition)
+    const username = usernameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+  
+    // VALIDATE inputs (USERNAME, EMAIL & PASSWORD)
+    if (!username || !email || !password) {
+      alert('Please fill in all fields');
+      return;
+    }
+  
+    //User data object
+    const userData = {
+      username: username,
+      email: email,
+      password: password
+    };
+  
+    // SEND data to server using fetch
+    // UPDATE URL with server endpoint
+    fetch('/signup', {
+      method: 'POST',
+      headers: {
+        //RENAME accordingly
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+    .then(response => {
+      // SERVER RESPONSE
+      if (response.ok) {
+
+        // SUCCESSFUL sign up displays message & redirect
+        alert('Signup successful!');
+      
+      } else {
+
+        // HANDLE error if signup fails
+        alert('Signup failed. Please try again.');
+      }
+    })
+    .catch(error => {
+        
+      // HANDLE all errors
+      console.error('Error:', error);
+      alert('An error occurred. Please try again later.');
+    });
+  }
+  
+  // EVENT LISTENER (NEW addition)
+  const signupForm = document.getElementById('signupForm');
+  signupForm.addEventListener('submit', handleSignup);
+  
+
+
+
+
+/*document.getElementById('signupForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
 
     // Get form input values
@@ -44,9 +111,5 @@ document.getElementById('signupForm').addEventListener('submit', function(event)
         console.error('Error:', error);
     });
 });
-
-
-
-
-
+*/
 
